dbba4e50d2b42561388860393b0dc9d6d28cb67f
John <jomcode@gmail.com>
2016-04-22 21:53:51 -0500

    passing invalid parameter test

--- index.js
+++ index.js
@@ -3,9 +3,27 @@ const moment = require('moment');
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => res.end());
+const timestamp = (url) => {
+  if (!url || url.length <= 1) return { unix: null, natural: null };
+  const naturalFormat = 'MMMM D, YYYY';
 
-server.listen(port, (err) => {
+  const unixToNatural = (uts) => moment.unix(uts).format(naturalFormat);
+  const naturalToUnix = (nts) => 'Invalid Date';
+
+  if (unixToNatural(url).toLowerCase() === 'invalid date' &&
+    naturalToUnix(url).toLowerCase() === 'invalid date') {
+    return { unix: null, natural: null };
+  }
+};
+
+const server = http.createServer((req, res) => {
+  const result = timestamp(req.url);
+  res.end(JSON.stringify(result));
+});
+
+const startServer = () => server.listen(port, (err) => {
   if (err) return console.error(err);
   console.log(`server listening on port ${port}`);
 });
+
+module.exports.timestamp = timestamp;
--- test/timestamp.test.js
+++ test/timestamp.test.js
@@ -1,5 +1,23 @@
-describe('timestamp', () => {
-  describe('handler', () => {
-    it('');
+const expect = require('chai').expect;
+const path = require('path');
+
+const timestamp = require('../index').timestamp;
+
+describe('timestamp microservice', () => {
+  describe('request handler', () => {
+    it('returns an object with `unix` and `natural` properties', () => {
+      expect(timestamp()).to.have.property('unix');
+      expect(timestamp()).to.have.property('natural');
+    });
+
+    it('`unix` and `natural` values are null if passed invalid date', () => {
+      const foo = timestamp('not a valid date');
+      expect(foo).to.have.property('unix', null);
+      expect(foo).to.have.property('natural', null);
+
+      const bar = timestamp('');
+      expect(bar).to.have.property('unix', null);
+      expect(bar).to.have.property('natural', null);
+    });
   });
 });
